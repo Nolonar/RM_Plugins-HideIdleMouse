187a258d933a64a3a8e44ff53dbdbb742559bd72
Nolonar <69093381+Nolonar@users.noreply.github.com>
2020-08-23 14:36:18 +0200

    add MIT License to js file

--- N_HideIdleMouse.js
+++ N_HideIdleMouse.js
@@ -1,3 +1,27 @@
+/* 
+ * MIT License
+ * 
+ * Copyright (c) 2020 Nolonar
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 //==============================================================================
 // N_HideIdleMouse
 //==============================================================================
@@ -27,7 +51,8 @@
     let timeoutDelay = Number(parameters.timeout) || 3000;
 
     let mouseIdleTimeout = null;
-    let defaultCursor = document.body.style.cursor;
+    let style = document.body.style;
+    let defaultCursor = style.cursor;
     hideMouse(); // Hidden by default.
 
     let window_onmousemove = window.onmousemove;
@@ -41,9 +66,9 @@
     };
 
     function showMouse() {
-        document.body.style.cursor = defaultCursor;
+        style.cursor = defaultCursor;
     };
     function hideMouse() {
-        document.body.style.cursor = 'none';
+        style.cursor = 'none';
     };
 })();
